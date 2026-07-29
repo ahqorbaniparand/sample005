de2f55831fa62613811a855d332f8f56db7e5cf7
ahqorbaniparand <ahqorbani.parand@gmail.com>
2026-07-29 16:08:02 +0330

    add scroll progress

--- js/script.js
+++ js/script.js
@@ -360,4 +360,18 @@ document.addEventListener("DOMContentLoaded", () => {
     cursor.style.left = e.clientX + "px";
     cursor.style.top = e.clientY + "px";
   });
+
+  // scroll progress
+  const progress = document.querySelector(".scroll-progress");
+
+  window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY;
+
+    const docHeight =
+      document.documentElement.scrollHeight - window.innerHeight;
+
+    const percent = (scrollTop / docHeight) * 100;
+
+    progress.style.width = percent + "%";
+  });
 });
