0a434bdae7dc1c140be47c44a76606bb1ef0d1a1
ahqorbaniparand <ahqorbani.parand@gmail.com>
2026-08-02 19:12:16 +0330

    chnage icon

--- js/script.js
+++ js/script.js
@@ -399,4 +399,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     skillBars.forEach((bar) => observer.observe(bar));
   }
+
+  // FADE IN ANIMATION
+  const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.add("show");
+    observer.unobserve(entry.target);
+  });
+}, {
+  threshold: 0.15
+});
+
+document.querySelectorAll(".fade-up").forEach(el => {
+  observer.observe(el);
+});
 });
